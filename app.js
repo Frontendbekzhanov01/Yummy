251f7a59b5335f09ceb4a2f2dc93870252bd2c62
Frontendbekzhanov01 <begjanovrasul1227@gmail.com>
2023-02-08 17:22:45 +0500

    sources

---
+++ app.js
@@ -0,0 +1,47 @@
+let btns = document.querySelectorAll(".menu button");
+
+btns.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        removeclass();
+        remove();
+        addactive(index);
+        item.classList.add("active");
+    });
+});
+
+function removeclass(){
+    btns.forEach((item) => {
+        item.classList.remove("active");
+    });
+}
+
+let pages = document.querySelectorAll(".main div");
+
+function remove() {
+    pages.forEach((item,index) => {
+        item.classList.remove("active");
+    });
+}
+
+function addactive(value){
+    pages.forEach((item,index) => {
+        if(value == index){
+            item.classList.add("active");
+        }
+    });
+}
+
+let formenu = document.querySelectorAll(".foods div");
+
+formenu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        removechangecol();
+        item.classList.add("changecol");
+    });
+});
+
+function removechangecol(){
+    formenu.forEach((item) => {
+        item.classList.remove("changecol");
+    });
+}
